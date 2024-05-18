60b8460afedbd8cccacc4b87fbee6829373a77c5
Jérôme Gangneux <jerome@gangneux.net>
2024-05-18 10:53:03 +0200

    feat: add compass info onto the GPS point

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,6 @@
 import './styles/bootstrap-5.3/css/bootstrap.css';
 import './styles/leaflet.css';
 import './styles/leaflet-override.css';
-import 'leaflet-compass/dist/leaflet-compass.src.css';
 import './styles/app.css';
 
 // start the Stimulus application
